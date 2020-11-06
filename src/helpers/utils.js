34fe92f7d5db447536d9eb8036e7e6c167f6ae19
Rafael Vieira <rafaellvs322@gmail.com>
2020-11-06 18:32:06 -0300

    ENH: improve utils file

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,57 +1,81 @@
+import cheerio from 'cheerio'
+
 // converts attributes to snake_case and deals with edge cases
 export const formatAttribute = string => {
-  if (string === 'NPC') return 'npc_price'
+  if (string.includes('NPC')) return 'npc_price'
   if (string === 'Rune Group') return 'group'
   if (string === 'Level') return 'lvl'
 
   return string
-    ? string.replace(/\n/g, '').replace(/\./g, '').replace(/\s/g, '_').replace(/%/g, '_mod').toLowerCase()
+    ? string
+      .replace(/\n/g, '')
+      .replace(/\./g, '')
+      .replace(/\s/g, '_')
+      .replace(/%/g, '_mod')
+      .toLowerCase()
     : 'image'
 }
+
+// strip html tags
+export const stripHtml = string =>
+  string
+    .replace(/(<([^>]+)>)/g, '')
+    .replace('&apos;', '\'')
+    .replace('\n', '')
+
+// handle name - removes <a> tag
+export const handleName = string =>
+  stripHtml(string)
 
 export const handleImage = string =>
   string.trim() && string.match(/https:.*?"/)[0].replace(/"|amp;/g, '')
 
-// scraper gets a string like 'ElfOrcOrc SpearmanTroll-Trained Salamander'
-// this treats it
-export const handleDroppedBy = string => {
-  const droppedBy = string
-    .replace(/([a-z])([A-Z])/g, '$1,$2')
-    .trim()
-    .split(',')
+// gets ul returned from loottable, loops through <li> and fill droppedBy array
+export const handleLootTable = string => {
+  const $ = cheerio.load(string)
+  const lootList = $('li')
+  const droppedBy = []
+
+  lootList.each(function (i, li) {
+    droppedBy.push($(li).text())
+  })
 
   return (
-    droppedBy[0] === 'This item is not dropped by any creatures.'
+    droppedBy[0] === 'This item is not dropped by any creatures.' ||
+    droppedBy[0] === 'This creature drops no loot.'
       ? null
       : droppedBy
   )
 }
 
-export const handleLoot = array => {
-  if (!Array.isArray(array)) return null
-
-  const loot = array.map(item => item.trim())
-
-  return (
-    array[0] === 'This creature drops no loot.'
-      ? null
-      : loot
-  )
-}
-
 export const handleEffect = string =>
-  string.replace(/<img.*>/g, '').trim()
+  stripHtml(string)
 
 export const handlePrem = string =>
   string === '\u2713' ? 'yes' : 'no'
 
 // format item data edge cases
 export const handleItemData = (attribute, value) => {
-  if (attribute === 'dropped_by') return handleDroppedBy(value)
-  else if (attribute === 'image') return handleImage(value)
-  else if (attribute === 'loot') return handleLoot(value)
-  else if (attribute === 'effect') return handleEffect(value)
-  else if (attribute === 'prem') return handlePrem(value)
+  switch (attribute) {
+    case 'name':
+      return handleName(value)
 
-  return value ? value.trim() : null
+    case 'dropped_by':
+      return handleLootTable(value)
+
+    case 'image':
+      return handleImage(value)
+
+    case 'loot':
+      return handleLootTable(value)
+
+    case 'effect':
+      return handleEffect(value)
+
+    case 'prem':
+      return handlePrem(value)
+
+    default:
+      return value ? stripHtml(value).trim() : null
+  }
 }
